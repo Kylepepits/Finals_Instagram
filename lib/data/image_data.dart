cecdec4daa623e3d983515d00b7fc98b8401c87f
Reigneir <reneir.panares@usjr.edu.ph>
2022-12-15 22:34:46 +0800

    added data for pictures

---
+++ lib/data/image_data.dart
@@ -0,0 +1,80 @@
+class ImageData {
+  final String id;
+  final String imageUrl;
+
+  const ImageData({
+    required this.id,
+    required this.imageUrl,
+  });
+}
+
+const imageList = [
+  ImageData(
+    id: 'id-001',
+    imageUrl:
+        'https://i.ibb.co/qjxWPbt/319874308-934243284261360-7044900356224948769-n.jpg',
+  ),
+  ImageData(
+    id: 'id-002',
+    imageUrl:
+        'https://i.ibb.co/s1rtP2y/319703556-1231514367464468-3710905347929046350-n.jpg',
+  ),
+  ImageData(
+    id: 'id-003',
+    imageUrl:
+        'https://i.ibb.co/bWhV8Bz/319539770-844777599926720-98080454415083028-n.jpg',
+  ),
+  ImageData(
+    id: 'id-004',
+    imageUrl:
+        'https://i.ibb.co/858Bc56/319536552-865342101473996-6225215897356372000-n.jpg',
+  ),
+  ImageData(
+    id: 'id-005',
+    imageUrl:
+        'https://i.ibb.co/tXrzqCG/319508814-591817999326747-5699478826599498517-n.jpg',
+  ),
+  ImageData(
+    id: 'id-005',
+    imageUrl:
+        'https://i.ibb.co/zsc2ZXF/318744281-1309832389583742-7093049203719120786-n.jpg',
+  ),
+  ImageData(
+    id: 'id-005',
+    imageUrl:
+        'https://i.ibb.co/WzHnwcD/317974076-861592888592461-8282463972294145776-n.png',
+  ),
+  ImageData(
+    id: 'id-005',
+    imageUrl:
+        'https://i.ibb.co/X2K67RD/317656101-1200858334193535-3115975060143150716-n.jpg',
+  ),
+  ImageData(
+    id: 'id-005',
+    imageUrl: 'https://i.ibb.co/Yk4T7MK/iced-tea.jpg',
+  ),
+  ImageData(
+    id: 'id-005',
+    imageUrl: 'https://i.ibb.co/28jBvH4/mango-shake.jpg',
+  ),
+  ImageData(
+    id: 'id-005',
+    imageUrl: 'https://i.ibb.co/4KjXpPG/cucumber-lemon.jpg',
+  ),
+  ImageData(
+    id: 'id-005',
+    imageUrl: 'https://i.ibb.co/xgYWBS9/spicy-cauliflower.jpg',
+  ),
+  ImageData(
+    id: 'id-005',
+    imageUrl: 'https://i.ibb.co/x8gWysQ/brocolli-tikki.jpg',
+  ),
+  ImageData(
+    id: 'id-005',
+    imageUrl: 'https://i.ibb.co/4fbYBvc/cruchy-cheese-balls.jpg',
+  ),
+  ImageData(
+    id: 'id-005',
+    imageUrl: 'https://i.ibb.co/tHd1pFn/garlic-bread.jpg',
+  ),
+];
